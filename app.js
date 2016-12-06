b5b44dbc3a46938d34fdea3f3dcc31eb129653d3
DickNeoM <dickneom@yahoo.com>
2016-12-05 20:31:36 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var server = http.createServer();
+function control(petic, resp) {
+    resp.writeHead(200, {'content-type': 'text/plain'});
+    resp.write('Hola, Mundo!');
+    resp.end();
+}
+server.on('request', control);
+server.listen(8080);
+console.log("Servidor activo en: 8080");
